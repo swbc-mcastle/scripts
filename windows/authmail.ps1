72549ca988a3a28941434978d55af854f1fa4145
osmith <oaken.smith@protonmail.com>
2022-04-25 08:23:57 -0500

    trying to fix the authmail script

--- windows/authmail.ps1
+++ windows/authmail.ps1
@@ -5,7 +5,7 @@ $Recipient = (get-aduser $Approver).userprincipalname
 $Greeting = echo ("Good morning, `n`nIT department policy states that we must receive confirmation from the supervisor or above on the exact groups given to a new or transferring user. Below is a list of security groups and where the groups grant access to (bulleted beneath the group) for your review. The list is based off of the existing user you requested be referenced. Please respond concerning any changes you may want made to the source, as well as authorization to add the listed groups to your target user.`n`nSource being copied: $SourceUser`nTarget being created: $TargetUser")
 $Groups = Get-ADPrincipalGroupMembership $SourceUser
 $ApproveList = ($Groups).objectGUID | Get-ADGroup -prop * | select name,description | Format-List
-$Mailbody = $Greeting, $ApproveList
+$Mailbody = $Greeting,$ApproveList
 
 #$olFolderDrafts = 16
 $Outlook = New-Object -ComObject Outlook.Application
@@ -14,8 +14,8 @@ $Mail = $Outlook.CreateItem(0)
 #$Mail.IsBodyHTML = $True
 $Mail.to = $Recipient
 $Mail.subject = "Request for Approval"
-$Mail.Body = "$Greeting"
-$Message.Body
+$Mail.Body = $Mailbody
+#$Message.Body = "$Mailbody"
 $Mail.save()
 $Inspector = $Mail.GetInspector
 $Inspector.Display()
